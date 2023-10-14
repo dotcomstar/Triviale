3347846181ea2f1caf17408151b686fd57615c8c
dotcomstar <gamestar109@live.com>
2023-10-14 00:17:41 -0700

    Initial commit

---
+++ src/data/questions.ts
@@ -0,0 +1,54 @@
+export default [
+  {
+    questionId: 0,
+    question:
+      "Although this man received 73 electoral votes, he would not become president; a situation which most directly led to the introduction of the 12th amendment. As vice president, he presided over the impeachment trail of Samuel P. Chase. Later in his political career, he was accused of treason for supposedly wanting to set up a monarchy in western North America. This man may be better known, though, for an incident that occured when he was running for governor of New York in 1804, where a federalist political opponent sullied his name with many slanders and eventually made him lose the election. This is, FTP, what man who challenged Alexander Hamilton to a duel and killed him?",
+    answer: "Burr",
+    fullAnswer: "Aaron Burr",
+    url: "https://quizbowlpackets.com/1460/2009%20Yale%20Bulldog%20(High%20School).pdf",
+    category: "history",
+  },
+  {
+    questionId: 1,
+    question:
+      "Author Nathanael West died in a car accident the day after this author died, possibly due to his grief at his friend‘s passing. That death left his final novel based on the life of Irving Thalberg unfinished, though it was printed posthumously under the title The Last Tycoon. The relationship between psychologist Dick Diver and his patient and wife Nicole in one novel is based on this author‘s actual problems with his mentally unstable wife, Zelda. In another novel, the title character becomes a millionaire by bootlegging in order to win the love of a girl he met during World War I, Daisy Buchanan. FTP, who is this author of Tender is the Night and The Great Gatsby?",
+    answer: "Fitzgerald",
+    fullAnswer: "F. Scott Fitzgerald",
+    url: "https://quizbowlpackets.com/1460/2009%20Yale%20Bulldog%20(High%20School).pdf",
+    category: "literature",
+  },
+  {
+    questionId: 2,
+    question:
+      "After serving in the Social war under his father Strabo he was sent to clear Sicily and Africa of opposition in the ensuing Civil War. He then subdued more enemies of Sulla when he was sent to Spain, after which he returned to Rome and took much credit for putting down Spartacus' rebellion. After he was made consul, he was given the extraordinary command of clearing the Mediteranean of pirates, which he did sucessfully. He success would soon come to an end, however, at the hands of one of his fellow triumvirs. Losing the battle of Pharsalus, this is, FTP, what member of the first triumvirate who fought against Caesar for control of the Roman Empire",
+    answer: "Pompey",
+    url: "https://quizbowlpackets.com/1460/2009%20Yale%20Bulldog%20(High%20School).pdf",
+    category: "history",
+  },
+  {
+    questionId: 3,
+    question:
+      "After Trotsky announced his response to this, The Germans began advancing toward Petrograd once again, and Lenin threatened to resign over this measure. Largely because of this measure, Russia was excluded from the peace negotiations at Versailles. Ultimately, Russia had to give up one third of its railway network, 73% of its iron ore production, and 780,000 square kilometers of territory. Resulting in the cession of territory already occupied by the German army, including Poland, Lithuania, and Latvia, this was FTP, what treaty signed by the Bolshevik revolutionaries with Germany that took Russia out of World War I?",
+    answer: "Brest-Litovsk",
+    fullAnswer: "Treaty of Brest-Litovsk",
+    url: "https://quizbowlpackets.com/1460/2009%20Yale%20Bulldog%20(High%20School).pdf",
+    category: "history",
+  },
+  {
+    questionId: 4,
+    question:
+      "The most successful animal species, by this measure, is probably the Antarctic Krill, which accounts for 0.7% of the Earth's total for this measure. That estimate places them at five times the total for human beings, but only about a quarter that of crops. In aquatic systems, primary consumers often have more of this quantity than primary producers, because of the rapid turnover of phytoplankton. However, on land, the slower life cycles of plants mean that pyramids organized around this quantity are always widest at the base. FTP, name this measure from ecology, whose units are typically given in mass per unit area.",
+    answer: "biomass",
+    url: "https://quizbowlpackets.com/1460/2009%20Yale%20Bulldog%20(High%20School).pdf",
+    category: "science",
+  },
+  {
+    questionId: 5,
+    question:
+      'As a theorist, his concept of the "objective correlative" influenced New Criticism; as a playwright, his works include The Burial of the Dead and Murder in the Cathedral; but he is best known as a poet. An early precocious work includes references to Donne and Hamlet, as well as an epigraph from Dante, while later poems like "Journey of the Magi" and "Ash Wednesday" reflect a turn towards religion. His most famous reference however, was to The Canterbury Tales, in the famous opening line "April is the cruelest month". FTP name this Nobel Prize-winning poet of "The Love Song of J. Alfred Prufrock" and "The Waste-Land".',
+    answer: "Eliot",
+    fullAnswer: "T. S. Eliot",
+    url: "https://quizbowlpackets.com/1460/2009%20Yale%20Bulldog%20(High%20School).pdf",
+    category: "literature",
+  },
+];
