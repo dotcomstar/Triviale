0516b8cbde11a9a298c0c3e65ba294921b4ddb96
dotcomstar <gamestar109@live.com>
2023-10-17 12:52:52 -0700

    Update help dialog

--- src/components/HelpDialog.tsx
+++ src/components/HelpDialog.tsx
@@ -1,4 +1,10 @@
-import { DialogProps, DialogTitle, Typography } from "@mui/material";
+import {
+  DialogProps,
+  DialogTitle,
+  List,
+  ListItem,
+  Typography,
+} from "@mui/material";
 import CustomDialog from "./CustomDialog";
 
 export interface HelpDialogProps {
@@ -23,10 +29,21 @@ const HelpDialog = ({
       TransitionComponent={TransitionComponent}
       ariaDescribedBy="help-dialog-slide"
     >
-      <DialogTitle> How To Play</DialogTitle>
-      <Typography sx={{ m: 3, mt: 0 }}>
-        Guess the answer in 5 tries. Total of 3 questions per day.
+      <DialogTitle sx={{ fontWeight: "bold", fontSize: "28px", pb: 0 }}>
+        How To Play
+      </DialogTitle>
+      <Typography sx={{ m: 3, my: 0, fontSize: "20px" }}>
+        Guess each answer in 5 tries.
       </Typography>
+      <List sx={{ listStyleType: "disc", mx: 6, mt: 0, pb: 3 }}>
+        <ListItem sx={{ display: "list-item" }}>
+          Total of 3 questions per day.
+        </ListItem>
+        <ListItem sx={{ display: "list-item" }}>
+          The color of the tiles will change to show how close your guess was to
+          the word.
+        </ListItem>
+      </List>
     </CustomDialog>
   );
 };
