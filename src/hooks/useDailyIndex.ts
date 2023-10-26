95ca9fcc389cfb061343bf49edb314b67172763b
dotcomstar <gamestar109@live.com>
2023-10-26 15:39:42 -0700

    Add more questions

--- src/hooks/useDailyIndex.ts
+++ src/hooks/useDailyIndex.ts
@@ -14,7 +14,6 @@ const useDailyIndex = () => {
   );
   const len = questions.length;
   const positiveModIndex = (((index * QUESTIONS_PER_DAY) % len) + len) % len;
-  console.log(positiveModIndex);
   return positiveModIndex;
 };
 
