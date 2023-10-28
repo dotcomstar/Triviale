7725aa323573abfb690640a1f922892fbb6b392e
dotcomstar <gamestar109@live.com>
2023-10-27 21:33:04 -0700

    Update questions

--- src/hooks/useDailyIndex.ts
+++ src/hooks/useDailyIndex.ts
@@ -1,12 +1,12 @@
 import { QUESTIONS_PER_DAY } from "../constants/settings";
 import questions from "../data/questions";
 
-// 1 January 2023 Game Epoch
-export const firstGameDate = new Date(2023, 0);
+// 26 October 2023 Game Epoch
+export const firstGameDate = new Date(2023, 9, 26);
 export const periodInDays = 1;
 const oneDay = 1000 * 60 * 60 * 24; // One day Time in ms (milliseconds)
 // const presentDate = new Date();
-const presentDate = new Date(new Date().getTime() + 2 * oneDay); // Temp offset so new questions come the second day and not the first
+const presentDate = new Date(new Date().getTime() + 9 * oneDay); // Temp offset so new questions come the second day and not the first
 
 const len = questions.length;
 
