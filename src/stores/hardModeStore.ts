b1fd53e7e73c7db21df47b6b5480ad8d190add24
dotcomstar <gamestar109@live.com>
2023-11-07 12:33:57 -0800

    Persist hard mode

--- src/stores/hardModeStore.ts
+++ src/stores/hardModeStore.ts
@@ -1,14 +1,25 @@
 import { create } from "zustand";
 import { mountStoreDevtool } from "simple-zustand-devtools";
+import useDailyIndex from "../hooks/useDailyIndex";
 
 interface HardModeStore {
   hardMode: boolean;
   toggleHardMode: () => void;
 }
 
+const dailyIndex = useDailyIndex();
+const existingGuesses = localStorage.getItem("prevGame") || "{}";
+const pastGuesses = JSON.parse(existingGuesses);
+const fromToday = pastGuesses["pastOffset"] === dailyIndex;
+
 const useHardModeStore = create<HardModeStore>((set) => ({
-  hardMode: false,
-  toggleHardMode: () => set((state) => ({ hardMode: !state.hardMode })),
+  hardMode: localStorage.getItem("hardMode") === "true" && fromToday,
+  toggleHardMode: () => {
+    set((state) => {
+      localStorage.setItem("hardMode", (!state.hardMode).toString());
+      return { hardMode: !state.hardMode };
+    });
+  },
 }));
 
 if (process.env.NODE_ENV === "development")
