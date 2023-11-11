f981f21e8d8127adda687e240ee68e4080928d26
dotcomstar <gamestar109@live.com>
2023-11-10 16:26:21 -0800

    Add more questions

--- src/components/grid/GameRow.tsx
+++ src/components/grid/GameRow.tsx
@@ -18,52 +18,68 @@ const GameRow = ({ guess, statuses = [] }: GameRowProps) => {
   const answer = answerWithSpaces.replace(/\s+/g, "")!;
   const emptyCells = Array.from(Array(answer.length - guess.length));
   const theme = useTheme();
+  let numSpacesForward = 1;
 
   return (
     <Stack direction="row" justifyContent="center" alignItems="center">
-      {guess.map((letter, i) => (
-        <>
-          <Cell key={i} nthLetter={i + 1} value={letter} status={statuses[i]} />
-          {i < answer.length - 1 && (
-            <Box
-              key={`after ${i}`}
-              sx={{
-                width: answerWithSpaces[i + 1] === " " ? "10px" : "5px",
-                borderBottom: answerWithSpaces[i + 1] === " " ? 0 : 2,
-                borderColor:
-                  statuses[i] === theme.palette.success &&
-                  statuses[i + 1] === theme.palette.success
-                    ? statuses[i].main
-                    : statuses[i] === theme.palette.error ||
-                      statuses[i + 1] === theme.palette.error ||
-                      statuses[i] === theme.palette.warning ||
-                      statuses[i + 1] === theme.palette.warning
-                    ? "primary.dark"
-                    : "primary.light",
-              }}
+      {guess.map((letter, i) => {
+        let shouldSkip = false;
+        if (answerWithSpaces[i + numSpacesForward] === " ") {
+          shouldSkip = true;
+          numSpacesForward += 1;
+        }
+        return (
+          <>
+            <Cell
+              key={i}
+              nthLetter={i + 1}
+              value={letter}
+              status={statuses[i]}
             />
-          )}
-        </>
-      ))}
-      {emptyCells.map((_, i) => (
-        <>
-          <Cell key={i + guess.length} nthLetter={guess.length + i + 1} />
-          {i < answer.length - guess.length - 1 && (
-            <Box
-              key={`after ${i + guess.length}`}
-              sx={{
-                width:
-                  answerWithSpaces[guess.length + i + 1] === " "
-                    ? "10px"
-                    : "5px",
-                borderBottom:
-                  answerWithSpaces[guess.length + i + 1] === " " ? 0 : 2,
-                borderColor: "primary.dark",
-              }}
-            />
-          )}
-        </>
-      ))}
+            {i < answer.length - 1 && (
+              <Box
+                key={`after ${i}`}
+                sx={{
+                  width: shouldSkip ? "10px" : "5px",
+                  borderBottom: shouldSkip ? 0 : 2,
+                  borderColor:
+                    statuses[i] === theme.palette.success &&
+                    statuses[i + 1] === theme.palette.success
+                      ? statuses[i].main
+                      : statuses[i] === theme.palette.error ||
+                        statuses[i + 1] === theme.palette.error ||
+                        statuses[i] === theme.palette.warning ||
+                        statuses[i + 1] === theme.palette.warning
+                      ? "primary.dark"
+                      : "primary.light",
+                }}
+              />
+            )}
+          </>
+        );
+      })}
+      {emptyCells.map((_, i) => {
+        let shouldSkipEmpty = false;
+        if (answerWithSpaces[i + guess.length + numSpacesForward] === " ") {
+          shouldSkipEmpty = true;
+          numSpacesForward += 1;
+        }
+        return (
+          <>
+            <Cell key={i + guess.length} nthLetter={guess.length + i + 1} />
+            {i < answer.length - guess.length - 1 && (
+              <Box
+                key={`after ${i + guess.length}`}
+                sx={{
+                  width: shouldSkipEmpty ? "10px" : "5px",
+                  borderBottom: shouldSkipEmpty ? 0 : 2,
+                  borderColor: "primary.dark",
+                }}
+              />
+            )}
+          </>
+        );
+      })}
     </Stack>
   );
 };
