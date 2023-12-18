c2009e75eb72562afde40db84f4ccc54ac17ed84
Jet Lee <gamestar109@live.com>
2023-12-18 00:03:04 -0500

    Add more questions for flight

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -26,38 +26,36 @@ export interface Question {
   category: Category;
 }
 
-interface MondoDBQuestion {
+export interface MondoDBQuestion {
   _id: { $numberInt: string };
   questions: Question[];
 }
 
 export const mongoDBQuestions: MondoDBQuestion[] = [
   {
-    _id: { $numberInt: "20231215" },
+    _id: { $numberInt: "20231217" },
     questions: [
       {
-        category: "SCI",
-        answer: "tongue",
-        question:
-          "A “geographic” pattern of patches can appear on this organ due to leukoplakia. This organ’s frenulum is tied to another body part in ankylo-glossia, and a protruding one is symptomatic of Down syndrome. It is not a tail, but chameleons possess a prehensile one of these organs that is covered in spiny papillae to assist cats with grooming. Sublingual salivary glands and taste buds are associated with, for fice points, which muscular organ inside the mouth?",
-        url: "https://files.quizbowlpackets.com/2701/Replacements.pdf",
+        question: `This country celebrates its Independence Day annually on August 15. In 2023, this country had an estimated GDP (PPP) of $13 trillion. The national flower for this country is the lotus and the national fruit is the mango. Occupying approximately 1.3mil sq mi, this country located in South Asia has an estimated population of around 1.4 billion. For five points, name this country with where the Ganges river flows and the capital is New Delhi.`,
+        answer: "India",
+        category: GEOGRAPHY,
       },
       {
-        category: "GEO",
-        question:
-          "Oxfam was founded to alleviate a famine in this country. This country celebrates “No Day,” a holiday that commemorates a refusal to cooperate with Benito Mussolini, given by Prime Minister Ioannis Metaxas.  The Earl of Elgin looted marble sculptures from this country, and the British Museum refuses to repatriate them. Since 2007, a debt crisis has plagued, for five points, what country where the Parthenon stands in Athens?",
-        answer: "Greece",
-        url: "https://files.quizbowlpackets.com/2701/Replacements.pdf",
+        question: `This American singer, songwriter, and actress married and then divorced Dalton Gomez. According to the RIAA, all of this woman’s studio albums are certified platinum or higher. She rose to fame for playing Cat Valentine in the Nickelodeon television series Victorious and has released popular albums such as “Thank U, Next.” For five points, name this woman whose last name is a Starbucks drink size.`,
+        answer: "Ariana Grande",
+        category: POP_CULTURE,
       },
       {
-        question:
-          'Formed in Liverpool in 1960, this legendary British rock band is widely regarded as one of the most influential in the history of popular music. Hits like "Hey Jude," "Let It Be," and "A Hard Day\'s Night" are just a glimpse of their extensive discography. With members like John Lennon, Paul McCartney, George Harrison, and Ringo Starr, they achieved global fame and success. What is the name of this iconic group that played a pivotal role in shaping the landscape of rock and roll, and whose name sounds like an insect?',
-        answer: "The Beatles",
-        category: "POP",
+        question: `This statesman succeeded Benjamin Franklin in 1785 as Minister to France. This prominent Founding Father is featured on the $2 bill and died on July 4th, 1826 at his home in Monticello. As a Democratic-Republican politician, this man served as Secretary of State under Washington and Vice President under John Adams. For five points, name this third US President who authored the Declaration of Independence.`,
+        answer: "Jefferson",
+        fullAnswer: "Thomas Jefferson",
+        category: HISTORY,
       },
     ],
   },
 ];
+
+// console.log(JSON.stringify(mongoDBQuestions));
 
 const questions: Question[] = [
   {
@@ -129,22 +127,19 @@ const questions: Question[] = [
     category: SCIENCE,
   },
   {
-    question: `This leader had an affair with Margherita Sarfatti, who wrote a biography about him, but she left his country around the time his government published the Manifesto of Race and passed racial laws. When King Zog refused to renew the Treaty of Tirana, this person made Albania a protectorate. During the Abyssinia Crisis, this leader ignored the League of Nations and invaded Ethiopia. This person often used bound wood as a symbol for his political movement. Name this leader of Italy who led the Fascist Parties and was killed by Italians near the end of World War II.`,
-    answer: "Mussolini",
-    url: "https://files.quizbowlpackets.com/2324/01.pdf",
-    category: HISTORY,
-  },
-
-  {
-    question: `This quantity is proportional to current times magnetic field and can be measured when a charge-carrying object is subject to a magnetic field. This quantity can also be determined experimentally in a redox reaction using the Nernst equation. Kirchoff’s second law states that this quantity is net zero within a loop. This quantity in a capacitor is equal to the charge divided by the capacitance. For 5 points, name this quantity that is the difference of electrical potential between two points and can be found by multiplying current and resistance in a circuit.`,
-    answer: "Voltage",
-    url: "https://files.quizbowlpackets.com/2571/Packet%205.pdf",
-    category: SCIENCE,
+    question: `Their lead guitarist is known for his distinctive guitar solos, and they performed during the iconic Live 8 charity concert in 2005. Formed in 1965 in London, this band is often associated with progressive rock and psychedelic music. This band’s founding members include Syd Barrett, Nick Mason, Roger Waters, and Richard Wright. For five points, name the legendary rock band is known for their concept album "The Dark Side of the Moon" and hits like "Wish You Were Here"?`,
+    answer: "Pink Floyd",
+    category: POP_CULTURE,
   },
   {
-    question: `Back when radium paint was used to make luminous watches, the sulfide of this element was typically used with the paint. An ion of this element is central to both carbonic anhydrase and alcohol dehydrogenase. This metal is in the calamine mineral, which contains both its carbonate and its silicate.  The pyrithione of this element is used to treat skin conditions, including dandruff.  To prevent the rusting of steel or iron, a coating of this element is applied, which is called galvanization.  This element usually makes up about one-third of brass, with most of the rest being copper. Name this metal whose atomic symbol is “Zn”.`,
-    answer: "Zinc",
-    url: "https://files.quizbowlpackets.com/2797/2023-state-round-1.pdf",
+    question: `Tishaura Jones assumed mayorship of this city in April 20, 2021. This city was founded on February 14, 1764, by French fur traders Gilbert Antoine de St. Maxent, Pierre Laclède and Auguste Chouteau. In 1904, the city hosted the World's Fair and the Olympics, becoming the first non-European city to host the games. This city features a 630-foot-tall monument which was designed by the Finnish-American architect Eero Saarinen in 1947. The NFL Rams controversially returned to Los Angeles from this city in 2016. For the point, name this city, the largest metropolitan area in Missouri and the second largest in Illinois. `,
+    answer: "Saint Louis",
+    category: GEOGRAPHY,
+  },
+  {
+    question: `This monarch was the elder daughter of Prince Albert, duke of York, and his wife, Lady Elizabeth Bowes-Lyon. She was queen regnant of 32 sovereign states over the course of her lifetime and remained the monarch of 15 realms by the time of her death. This woman had four children: Charles, Anne, Andrew, and Edward. From the House of Windsor, this woman was born April 21, 1926, London, England and died September 8, 2022. Who is this longest reigning British Monarch, former Queen of England?    `,
+    answer: "Elizabeth",
+    fullAnswer: "Queen Elizabeth II",
     category: SCIENCE,
   },
   {
