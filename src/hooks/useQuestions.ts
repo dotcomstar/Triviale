4be0da72fd80b71b5e51dbc2e9fd62d78f453548
dotcomstar <gamestar109@live.com>
2023-12-30 13:59:59 -0500

    Remove MongoDB API calls in production

--- src/hooks/useQuestions.ts
+++ src/hooks/useQuestions.ts
@@ -7,26 +7,27 @@ export type QuestionsResponse = {
 };
 
 const useQuestions = (): QuestionsResponse => {
-  // Try to get questions with MongoDB API.
-  const setRetrieved = useRetrievedStore((s) => s.setRetrieved);
-  const { data: mongoQuestionsArray } = useMongoDBQuestions();
-  let mongoData = null;
-  if (mongoQuestionsArray) {
-    mongoData = mongoQuestionsArray[0];
-  }
+  return { data: questions };
+  // // Try to get questions with MongoDB API.
+  // const setRetrieved = useRetrievedStore((s) => s.setRetrieved);
+  // const { data: mongoQuestionsArray } = useMongoDBQuestions();
+  // let mongoData = null;
+  // if (mongoQuestionsArray) {
+  //   mongoData = mongoQuestionsArray[0];
+  // }
 
-  if (mongoData) {
-    // Successfully retrieved questions using MongoDB API.
-    console.log("Successfully retrieved questions using MongoDB.");
-    setRetrieved(true);
-    return { data: mongoData.questions };
-  } else {
-    // We were not able to get questions for today.
-    console.log("Unable to get MongoDB data. Using local questions backup.");
-    return {
-      data: questions,
-    };
-  }
+  // if (mongoData) {
+  //   // Successfully retrieved questions using MongoDB API.
+  //   console.log("Successfully retrieved questions using MongoDB.");
+  //   setRetrieved(true);
+  //   return { data: mongoData.questions };
+  // } else {
+  //   // We were not able to get questions for today.
+  //   console.log("Unable to get MongoDB data. Using local questions backup.");
+  //   return {
+  //     data: questions,
+  //   };
+  // }
 };
 
 export default useQuestions;
