a961ee94c803c5d0e43599e5a991e59898326130
dotcomstar <gamestar109@live.com>
2024-01-29 13:28:40 -0800

    Try svh

--- src/components/landingPage/LandingDialog.tsx
+++ src/components/landingPage/LandingDialog.tsx
@@ -45,8 +45,7 @@ const LandingDialog = ({
         justifyContent="space-between"
         alignItems="center"
         width={"100%"}
-        height={"100dvh"}
-        maxHeight={matches ? "100dvh" : "100dvh"}
+        height={"100svh"}
         spacing={matches ? undefined : "4%"}
         sx={{ mt: matches ? 20 : 8, mb: 8, px: 4 }}
       >
